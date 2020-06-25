3dced96232bb43972fcc2eed79d21f722ba5ee2e
Matthew Batchelder <borkweb@gmail.com>
2020-06-24 22:29:10 -0400

    Allow npm install on subdirectory use targets
    The problem with `npm install` on plugin subdirectories was that the presence of a `.git/` directory was _required_ to perform the git commands that npm was executing. Because `common/` is a submodule, it doesn't have a `.git/` directory...so when we were mounting common as `/project`, the `.git/` dir was not found, even when traversing up the tree.
    
    The solution was to—when running `tric use` against a submoduled subdirectory—set the parent directory as the volume and then `cd` into `common/` in the `docker-entrypoint.sh`.
    
    :movie_camera: http://p.tri.be/7dw7hH

--- containers/npm/docker-entrypoint.sh
+++ containers/npm/docker-entrypoint.sh
@@ -5,4 +5,17 @@
 # If the `FIXUID` env var is set to `1`, default value, then fix UIDs.
 test "${FIXUID:-1}" != "0" && eval "$( fixuid )"
 
-npm --prefix /project "$@"
+cd /project/${TRIC_CURRENT_PROJECT_SUBDIR}
+
+npm "$@"
+
+# Output error logs if present.
+if compgen -G "/home/node/.npm/_logs/*.log" > /dev/null; then
+  echo "---------------------------------------"
+  echo "Error log found. Here are the contents (excluding the overly verbose saveTree lines):"
+  echo "---------------------------------------"
+  cat /home/node/.npm/_logs/*.log | grep -v "saveTree"
+  echo "---------------------------------------"
+  echo "End of error log"
+  echo "---------------------------------------"
+fi
