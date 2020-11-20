817c3ec05c8fec7fb6cf033581d73444816f9cf8
Paul <kim.paul.ms@gmail.com>
2020-11-21 01:16:10 +0800

    remove logging from npm entry point

--- containers/npm/docker-entrypoint.sh
+++ containers/npm/docker-entrypoint.sh
@@ -8,14 +8,3 @@ test "${FIXUID:-1}" != "0" && eval "$( fixuid > /dev/null 2>&1 )"
 cd /project/${TRIC_CURRENT_PROJECT_SUBDIR}
 
 npm "$@"
-
-# Output error logs if present.
-if compgen -G "/home/node/.npm/_logs/*.log" > /dev/null; then
-  echo "---------------------------------------"
-  echo "Error log found. Here are the contents (excluding the overly verbose saveTree lines):"
-  echo "---------------------------------------"
-  cat /home/node/.npm/_logs/*.log | grep -v "saveTree"
-  echo "---------------------------------------"
-  echo "End of error log"
-  echo "---------------------------------------"
-fi
