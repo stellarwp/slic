088a317ba6bd36f8bb46608efa4d0594c58672dd
Matthew Batchelder <borkweb@gmail.com>
2021-01-19 22:20:39 -0500

    Add the composer-cache command
    This command allows you to map a composer cache directory into the tric containers so that `tric composer` commands can benefit from cached dependencies.
    
    This should help us speed up our GitHub Actions when we leverage `action/cache@v2`!
    
    :movie_camera: https://d.pr/v/mqG0Tk
    
    **Note:** I apologize in advance, my mind went blank partway through the video and I stuttered and stammered a bit while waiting for composer to fetch dependencies. Ultimately I don't care enough to re-record as it doesn't waste any time :D

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.5.22] - 2021-01-20
+### Added
+- Added the `composer-cache` command so the host machine's composer cache can be leveraged within tric containers.
+
 ## [0.5.21] - 2021-01-19
 ### Changed
 - Updated default WordPress image to `5.6-apache`.
---
+++ src/commands/composer-cache.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tribe\Test;
+
+if ( $is_help ) {
+	echo "Sets or displays the composer cache directory setting.\n";
+	echo PHP_EOL;
+	echo colorize( "signature: <light_cyan>{$cli_name} composer-cache [(set <dir>|unset)]</light_cyan>\n" );
+	echo colorize( "example: <light_cyan>{$cli_name} composer-cache</light_cyan>\n" );
+	echo colorize( "example: <light_cyan>{$cli_name} composer-cache /home/person/.cache/composer</light_cyan>\n" );
+	return;
+}
+
+$composer_cache_args = args( [ 'toggle', 'value' ], $args( '...' ), 0 );
+
+tric_handle_composer_cache( $composer_cache_args );
