39644604ff63a5b71513037537d556979133d5c8
Luca Tumedei <luca@theaveragedev.com>
2022-05-20 14:39:11 +0200

    doc(commands/logs) add missing doc-block

--- src/commands/logs.php
+++ src/commands/logs.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Opens and follows the stack logs.
+ *
+ * @var bool   $is_help  Whether we're handling an `help` request on this command or not.
+ * @var string $cli_name The current name of the `tric` CLI application.
+ */
 
 namespace TEC\Tric;
 
@@ -6,6 +12,7 @@ if ( $is_help ) {
 	echo "Displays the stack logs.\n";
 	echo PHP_EOL;
 	echo colorize( "usage: <light_cyan>{$cli_name} logs</light_cyan>" );
+
 	return;
 }
 
