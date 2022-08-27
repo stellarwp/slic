84a6cb38dddba1a1d76a92059e1e0e08440863ce
Matthew Batchelder <borkweb@gmail.com>
2022-08-27 15:03:01 -0400

    Adjust docs for the build-subdir command

--- src/commands/build-subdir.php
+++ src/commands/build-subdir.php
@@ -10,12 +10,24 @@
 namespace StellarWP\Slic;
 
 if ( $is_help ) {
-	echo "Activates or deactivates whether or not composer/npm build should apply to sub-directories.\n";
-	echo PHP_EOL;
-	echo colorize( "signature: <light_cyan>{$cli_name} build-subdir (on|off|status)</light_cyan>\n" );
-	echo colorize( "example: <light_cyan>{$cli_name} build-subdir on</light_cyan>\n" );
-	echo colorize( "example: <light_cyan>{$cli_name} build-subdir off</light_cyan>\n" );
-	echo colorize( "example: <light_cyan>{$cli_name} build-subdir status</light_cyan>\n" );
+	$help = <<< HELP
+	Activates or deactivates whether or not composer/npm build should apply to sub-directories.
+
+	Usage: <light_cyan>{$cli_name} {$subcommand} (on|off|status)</light_cyan>
+
+	Examples:
+
+	  <light_cyan>{$cli_name} {$subcommand} on</light_cyan>
+	    Enable application of composer/npm commands in sub-directories.
+
+	  <light_cyan>{$cli_name} {$subcommand} off</light_cyan>
+	    Disable application of composer/npm commands in sub-directories.
+
+	  <light_cyan>{$cli_name} {$subcommand} status</light_cyan>
+	    Show the current status of running composer/npm commands in sub-directories.
+	HELP;
+
+	echo colorize( $help );
 
 	return;
 }
