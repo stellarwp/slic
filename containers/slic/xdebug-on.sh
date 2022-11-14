c604a9671b5309795378c0e1964332e862a3bccf
Luca Tumedei <luca@theaveragedev.com>
2022-11-14 13:46:54 +0100

    build(containers/slic) xdebug start_with_request

--- containers/slic/xdebug-on.sh
+++ containers/slic/xdebug-on.sh
@@ -2,6 +2,3 @@
 xdebug_config_file=$(php --ini | grep xdebug | cut -d, -f1)
 sed -i '/^;zend_extension/ s/;zend_extension/zend_extension/g' "$xdebug_config_file"
 php -v
-echo ""
-echo "XDebug is \033[32mon\033[0m."
-echo ""
