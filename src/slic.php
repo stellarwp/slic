81969d52eaf71f96ed849e326a9cb42ba670a0e7
Luca Tumedei <luca@theaveragedev.com>
2025-11-28 10:06:52 +0100

    fix: set project path early during use command
    While the `use` command is being handled, the project path will still
    point at the `here` target, not the new target set by the `use` command.
    I.e. the `get_project_local_path()` function will return
    `/home/wp/wp-content/plugins` and not
    `/home/wp/wp-content/plugins/the-events-calendar`.
    From this follows that the project configuration files will be read from
    the `/home/wp/wp-content/plugins` directory and not from the
    `/home/wp/wp-content/plugins/the-events-calendar` one invlalidating the
    effect of some of the env vars defined in files like the
    `.env.slic.local` that might be present in the project directory
    (`the-events-calendar` in this case).
    By setting the project path early during a `use` command handling, env
    vars  that affect the docker stack (e.g. the `SLIC_DB_NO_MIN` one) will
    correctly have the desired effect.

--- src/slic.php
+++ src/slic.php
@@ -272,6 +272,20 @@ function setup_slic_env( $root_dir, $reset = false ) {
 		load_env_file( $root_dir . '/.env.slic.run' );
 	}
 
+	/*
+	 * Special handling of the `use` command.
+	 *
+	 * This has to be detected early to allow for override files in the `use` target to apply.
+	 * If the user called `slic use some-plugin`, then this will set the `SLIC_CURRENT_PROJECT` env
+	 * var to `some-plugin`. This will, in turn, affect all the functions that use the information of the
+	 * project path like the `get_project_local_path()` one in the context of the handling of the `use` command.
+	 */
+	global $argc, $argv;
+	if ( $argc >= 3 && $argv[1] === 'use' && ! empty( $argv[2] ) ) {
+		$target = (string) ensure_valid_target( $argv[2] );
+		putenv( "SLIC_CURRENT_PROJECT={$target}" );
+	}
+
 	$target_path = get_project_local_path();
 	if( ! empty( $target_path ) ) {
 		// Load the local overrides from the target.
